21086cc25a1acbdc63726046c2b258ad672720f2
Simon Charters <simon259@hotmail.co.uk>
2021-01-11 15:50:31 +0000

    tasks displaying and can now add new tasks

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 import Vue from 'vue';
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Javascript Loaded")
-})
+    new Vue({
+        el: '#app',
+        data: {
+            tasks: ['Walk Dog', 'Wash Car', 'Do Homework'],
+       
+        newTask: ""
+    },
+    methods: {
+        saveNewTask: function() {
+            this.tasks.push(this.newTask);
+            this.newTask = ""
+        }
+    }
+    })
+});
